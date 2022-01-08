f0d6103ef7e9b3a10158c0beb0c5e302de626520
Richie-Yang <richieyang8813@gmail.com>
2022-01-08 15:59:44 +0800

    feat: make icon display apporiately and sligtly adjust index view

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -6,14 +6,6 @@ const db = require('../../config/mongoose')
 const Category = require('../category')
 const { SEED_CATEGORIES } = require('../../dummyData.json')
 
-const CATEGORY = {
-  家居物業: "https://fontawesome.com/icons/home?style=solid",
-  交通出行: "https://fontawesome.com/icons/shuttle-van?style=solid",
-  休閒娛樂: "https://fontawesome.com/icons/grin-beam?style=solid",
-  餐飲食品: "https://fontawesome.com/icons/utensils?style=solid",
-  其他: "https://fontawesome.com/icons/pen?style=solid"
-}
-
 
 db.once('open', async() => {
   try {
@@ -38,14 +30,7 @@ db.once('open', async() => {
     console.log('\nstart generating new seed categories in database')
     await Promise.all(
       Array.from(Array(SEED_CATEGORIES.length), (_, i) => {
-        const { name } = SEED_CATEGORIES[i]
-        let icon = ''
-
-        for (let item in CATEGORY) {
-          if (item === name) icon = CATEGORY[item]
-          console.log(icon)
-        }
-
+        const { name, icon } = SEED_CATEGORIES[i]
         return Category.create({ icon, name })
       })
     ).then(() => {
