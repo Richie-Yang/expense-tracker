3e07d97d3c2c439b2b1fa7327bddcf8b26541ccd
Richie-Yang <richieyang8813@gmail.com>
2022-01-11 22:25:42 +0800

    feat: add text description for app.js

--- app.js
+++ app.js
@@ -15,22 +15,34 @@ const { engine } = require('express-handlebars')
 const PORT = process.env.PORT
 require('./config/mongoose')
 
+// set handlebars as template engine
 app.engine('hbs', engine({ 
   defaultLayout: 'main', 
   extname: '.hbs', 
   helpers: require('./config/handlebars')
 }))
 app.set('view engine', 'hbs')
+
+
+////////// route section starts here //////////
+// use express-session module
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
+
+// use passport module
 usePassport(app)
+// use method-override module
 app.use(methodOverride('_method'))
+// use built-in body-parser module
 app.use(express.urlencoded({ extended: true }))
+// use static routing
 app.use(express.static('public'))
+// use connect-flash module
 app.use(flash())
+// use custom middleware to preload req data into res
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
@@ -41,10 +53,12 @@ app.use((req, res, next) => {
   next()
 })
 app.use(routes)
+// use custom middleware to deal with error exception
 app.use((err, req, res, next) => {
   console.log(err.stack)
   return serverError(res)
 })
+////////// route section ends here //////////
 
 
 app.listen(PORT, () => {
